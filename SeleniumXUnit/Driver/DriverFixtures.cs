45d0529248ff84942c42c58a15bf84900900901a
alexavalencia <alexavalencia94@hotmail.com>
2022-12-22 09:57:20 -0500

    xunit dependency injection

---
+++ SeleniumXUnit/Driver/DriverFixtures.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Autofac;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumXUnit.Driver;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumXUnit
+{
+    public class DriverFixtures : IDriverFixtures
+    {
+        IWebDriver driver;
+        private readonly IBrowserDriver browserDriver;
+
+        public DriverFixtures(IBrowserDriver browserDriver)
+        {
+            this.browserDriver = browserDriver;
+            driver = GetWebDriver();
+        }
+
+        public IWebDriver Driver => driver;
+        private IWebDriver GetWebDriver(BrowserType browserType = BrowserType.Chrome)
+        {
+
+
+            return browserType switch
+            {
+                BrowserType.Chrome => browserDriver.GetChromeDriver(),
+                BrowserType.Firefox => browserDriver.GetFirefoxDriver(),
+                _ => browserDriver.GetChromeDriver()
+            };
+
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
+
