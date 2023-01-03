19cbf08a82ae597337b953ba32bb503a4d90e79d
alexavalencia <alexavalencia94@hotmail.com>
2023-01-03 15:45:23 -0500

    added docker run script file

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,9 +9,9 @@ export COMPOSE_HTTP_TIMEOUT=200
 docker-compose -p "$project" build
 docker-compose -p "$project" up -d ea_api ea_webapp db ea_test node-docker selenium-hub
 docker-compose -p "$project" up --no-deps ea_test
-exit_code=$(docker inspect ea_test -f '{{.State.ExitCode}}')
+exit_code=$(docker inspect ea_test -f '{{ .State.ExitCode }}')
 
-if[$exit_code -eq 0]; then
+if [ $exit_code -eq 0 ]; then
     exit $exit_code
 else
     echo "Test failed"
