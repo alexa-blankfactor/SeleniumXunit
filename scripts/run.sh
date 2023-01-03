67645a35506c2897a398542983046bededc36735
alexavalencia <alexavalencia94@hotmail.com>
2023-01-03 16:14:23 -0500

    adding support for report as artifact

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,9 +13,6 @@ mkdir -m 777 reports
 docker-compose -p "$project" up -d ea_api ea_webapp db ea_test node-docker selenium-hub
 docker-compose -p "$project" up --no-deps ea_test
 
-docker cp ea_test:/src/TestBdd/LivingDoc.html ./reports
-echo "Specflow living doc reports is copied to ./reports"
-ls -l ./reports
 
 exit_code=$(docker inspect ea_test -f '{{ .State.ExitCode }}')
 
@@ -24,5 +21,9 @@ if [ $exit_code -eq 0 ]; then
 else
     echo "Test failed"
 fi 
+
+docker cp ea_test:/src/TestBdd/LivingDoc.html ./reports
+echo "Specflow living doc reports is copied to ./reports"
+ls -l ./reports
 
 
